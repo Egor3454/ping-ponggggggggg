94dd666ec454624131ce40009414a0ef73c3563b
egor <118670195+Egor3454@users.noreply.github.com>
2022-11-20 17:14:25 +0300

    Add files via upload
    открывается синий фон

---
+++ shooter_game.py
@@ -0,0 +1,19 @@
+#Создай собственный Шутер!
+
+from pygame import *
+from random import *
+from time import time as timer
+b = (200,255,255)
+win_width = 600
+win_height = 500
+w = display.set_mode((win_width,win_height))
+w.fill(b)
+display.set_caption('ping-pong')
+c = time.Clock()
+r = True
+while r:
+    for e in event.get():
+        if e.type == QUIT:
+            r = False
+    display.update()
+    c.tick(60)
