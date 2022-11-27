47208351d2c22d79eff4630da596580d5c6a5146
egor <118670195+Egor3454@users.noreply.github.com>
2022-11-27 16:25:06 +0300

    1.3
    появились игроки

--- shooter_game.py
+++ shooter_game.py
@@ -23,18 +23,25 @@ class Player(GameSprite):
         kp = key.get_pressed()
         if kp[K_UP] and self.rect.y>0:
             self.rect.y -= self.speed
-        if kp[K_DOWN] and self.rect.y<500:
+        if kp[K_DOWN] and self.rect.y<400:
             self.rect.y += self.speed
 class Player2(GameSprite):
     def update(self):
         kp = key.get_pressed()
-        if kp[K_W] and self.rect.y>0:
+        if kp[K_w] and self.rect.y>0:
             self.rect.y -= self.speed
-        if kp[K_S] and self.rect.y<500:
+        if kp[K_s] and self.rect.y<400:
             self.rect.y += self.speed
+p1 = Player('racketka.jpg',515,400,80, 100, 10)
+p2 = Player2('racketka.jpg',5,400,80, 100, 10)
 while r:
     for e in event.get():
         if e.type == QUIT:
             r = False
+    w.fill(b)      
+    p1.update()
+    p1.reset()
+    p2.update()
+    p2.reset()
     display.update()
     c.tick(60)
